2ea9cb9014105d7c711795ad1864d718ee5cd544
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-22 21:14:10 -0800

    added language toggle buttons so users can filter projects by language

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const projectArray = [
     then share with friends, and which can be canceled at the user’s discretion or will expire in 12 hours. Users
     can also schedule a “rescue call” from the app, which will dial them at a time of their choice, providing them
     a convenient escape in case their date turns sour.`,
-    languages: "React, Express, Node.js, MySQL",
+    languages: "React.js, Bootstrap, Express, Node.js, MySQL",
     github: "https://github.com/github-marti/Date-Rescue",
     deployed: "https://date-rescue.herokuapp.com/"
   },
@@ -15,7 +15,7 @@ const projectArray = [
     title: "WordLove",
     images: ["assets/images/wordlove/wordlove-1.png", "assets/images/wordlove/wordlove-2.png", "assets/images/wordlove/wordlove-3.png", "assets/images/wordlove/wordlove-4.png", "assets/images/wordlove/wordlove-5.png"],
     description: "Vocabulary learning app that allows users to look up words and phrases in other languages and save them to their own custom vocabulary lists. Users can even practice their saved words with flashcards.",
-    languages: "HTML, CSS, JavaScript, Node.js, MySQL",
+    languages: "HTML/CSS, JavaScript, jQuery, Express, Node.js, MySQL",
     github: "https://github.com/github-marti/WordLove",
     deployed: "https://wordlove.herokuapp.com/"
   },
@@ -23,7 +23,7 @@ const projectArray = [
     title: "Musiholic",
     images: ["assets/images/musiholic/musiholic-1.png", "assets/images/musiholic/musiholic-2.png"],
     description: "Looking for the perfect cocktail and music combination? Sit back, relax, and open this app! Enter the ingredients you have on hand, your drink of choice, or select a music genre you're feeling and we'll see what we can whip up for you.",
-    languages: "Vue.js",
+    languages: "Vue.js, Bootstrap",
     github: "https://github.com/github-marti/Musiholic",
     deployed: "https://github-marti.github.io/Musiholic/"
   },
@@ -31,7 +31,7 @@ const projectArray = [
     title: "Weather Dashboard",
     images: ["assets/images/weather-dashboard.png"],
     description: "Want to check your local weather forecast? No problem! This app shows you the local weather conditions and allows you to search for the weather in the city of your choice.",
-    languages: "HTML, CSS, JavaScript",
+    languages: "HTML/CSS, JavaScript, jQuery",
     github: "https://github.com/github-marti/Weather-Dashboard",
     deployed: "https://github-marti.github.io/Weather-Dashboard/"
   },
@@ -39,7 +39,7 @@ const projectArray = [
     title: "Team Profile Generator",
     images: ["assets/images/team-summary-generator.png"],
     description: "Node CLI application that generates a PDF 'Team Profile' based on the user's command line input.",
-    languages: "HTML, Node.js",
+    languages: "HTML/CSS, Node.js",
     github: "https://github.com/github-marti/Template-Engine---Team-Summary",
     deployed: "https://drive.google.com/file/d/1TSojiEpSEBctz4BQWzw6_JXx6T03dC6_/view"
   },
@@ -47,7 +47,7 @@ const projectArray = [
     title: "Note Taker",
     images: ["assets/images/note-taker.png"],
     description: "Express application that allows users to save, update, and delete notes using a mySQL database.",
-    languages: "HTML, CSS, JavaScript, Node.js, MySQL",
+    languages: "HTML/CSS, JavaScript, jQuery, Express, Node.js, MySQL",
     github: "https://github.com/github-marti/Note-Taker",
     deployed: "https://martis-note-taker.herokuapp.com/"
   }
@@ -95,6 +95,23 @@ $(document).scroll(function () {
 // make navbar sticky once scrolling down past header
 window.onscroll = function () { stickyNavBar() };
 
+// filter projects based on selected language
+$('.lang-title').on('click', function(event) {
+  if (event.target.textContent === 'All') {
+    $('.project-wrapper').show();
+  } else {
+    // get array of projects that use selected language
+    const toHide = projectArray.filter(e => !e.languages.includes(event.target.textContent)).map(e => e.title);
+    const toShow = projectArray.filter(e => e.languages.includes(event.target.textContent)).map(e => e.title);
+    toHide.forEach(p => {
+      $(`#${p.toLowerCase().replace(/\s/g, '-')}`).hide();
+    });
+    toShow.forEach(p => {
+      $(`#${p.toLowerCase().replace(/\s/g, '-')}`).show();
+    });
+  }
+})
+
 // make modal appear when project image is clicked
 $('.project-wrapper').click(function () {
   let modalIndex = $(this).attr('modal-index');
@@ -118,5 +135,5 @@ $('.project-wrapper').click(function () {
 // make modal disappear when screen is narrow
 $('.close').click(function () {
   $('.modal').attr('style', 'display:hidden;');
-})
+});
 
