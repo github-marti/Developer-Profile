f20c1b2c35323dfa53e404f48768d2da6397da72
Marti <52050778+github-marti@users.noreply.github.com>
2020-02-09 17:08:26 -0800

    adding webpack for babel and future development

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './assets/js/script.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
